45eaf22d79efcc74fa8984b9c18f9c0cdad739f2
seongmin <sm4046@naver.com>
2026-05-11 01:38:19 +0900

    [#15]Revert: updateProfile API 및 useUpdateProfile 훅 제거 (PUT API 미지원)

--- src/apis/modules/myBusiness.ts
+++ src/apis/modules/myBusiness.ts
@@ -3,8 +3,6 @@ import { ENDPOINTS } from "@/apis/endpoints";
 
 export const myBusinessApi = {
   getProfile: () => http.get(ENDPOINTS.myBusiness.profile),
-  updateProfile: (data: Record<string, unknown>) =>
-    http.put(ENDPOINTS.myBusiness.profile, data),
   getPartners: (params?: { page?: number; limit?: number }) =>
     http.get(ENDPOINTS.myBusiness.partners, { params }),
   savePartner: (data: { partnerId: string; partnerType: "seller" | "buyer" }) =>
