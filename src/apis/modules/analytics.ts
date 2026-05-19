66d0621a77f1a5069626550e00ce59e4c7d24d44
seongmin <sm4046@naver.com>
2026-05-19 16:13:52 +0900

    [#26]Remove: 미사용 API 모듈 7종 삭제
    - admin, analytics, buyers, companies, consultations, matches, payments 모듈 제거
    - apis/index.ts에서 re-export 제거
    - apis/endpoints.ts에서 관련 엔드포인트 정의 제거 (auth/partners/myBusiness/escrowPayments만 유지)

--- src/apis/modules/analytics.ts
+++
@@ -1,7 +0,0 @@
-import { http } from "@/apis/http";
-
-export const analyticsApi = {
-  dashboard: () => http.get("/analytics/dashboard"),
-  topIndustries: () => http.get("/analytics/industries/top"),
-  recentTransactions: () => http.get("/analytics/transactions/recent"),
-};
